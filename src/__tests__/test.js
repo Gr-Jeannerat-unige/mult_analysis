a64d56f1681a51d38a4dab18f208630f401353fe
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-03-22 18:37:53 +0100

    update

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -2,6 +2,7 @@ import { myModule } from '..';
 
 describe('test myModule', () => {
   it('should return 42', () => {
-    expect(myModule()).toEqual(42);
+    const result = myModule();
+    expect(result.checkvalue).toStrictEqual(42);
   });
 });
