6154cd8c9577751a5c1a9169047914b5cc29a76d
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-03-22 22:35:47 +0100

    feat: added web page for all multiplet of a spectrum

---
+++ examples/demo1.js
@@ -0,0 +1,19 @@
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+import doublet from '../data/dd-exp2.json';
+import { myModule } from '../src/index';
+
+const result = myModule(doublet, {
+  frequency: 600,
+  debug: true,
+  takeBestPartMultiplet: true,
+  appliedPhaseCorrectionType: 1,
+});
+
+writeFileSync(
+  join(__dirname, '../web', 'result.json'),
+  JSON.stringify(result, null, 2),
+  'utf8',
+);
+console.log(result);
---
+++ examples/demo_mn.js
@@ -0,0 +1,63 @@
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+import data from '../St 290-JK.json';
+import { myModule } from '../src/index';
+
+
+//console.log(data.multiplets);
+console.log(data);
+
+console.log(data.multiplets.length);
+let fromPt, toPt;
+let x = [];
+let y = [];
+const increment =
+  (data.mnFullScaleLimits.toX - data.mnFullScaleLimits.fromX) /
+  data.mnReal.length;
+let result;
+let results = [];
+for (let i = 0; i < data.multiplets.length; i++) {
+  fromPt =
+    data.mnReal.length *
+    (Math.abs(data.multiplets[i].rangeMax - data.mnFullScaleLimits.toX) /
+      Math.abs(data.mnFullScaleLimits.toX - data.mnFullScaleLimits.fromX));
+  toPt =
+    data.mnReal.length *
+    (Math.abs(data.multiplets[i].rangeMin - data.mnFullScaleLimits.toX) /
+      Math.abs(data.mnFullScaleLimits.toX - data.mnFullScaleLimits.fromX));
+  fromPt = Math.round(fromPt);
+  toPt = Math.round(toPt);
+
+  y = data.mnReal.slice(fromPt, toPt);
+  //x = Array.apply(null, { length: y.length }).map(Function.call, (Number + 0.2));
+  x = Array.from(
+    { length: y.length },
+    (v, k) => increment * k + data.multiplets[i].rangeMin,
+  );
+  result = myModule(
+    { x, y },
+    {
+      frequency: 600,
+      debug: true,
+      takeBestPartMultiplet: true,
+      minimalResolution: 0.01,
+      appliedPhaseCorrectionType: 0,
+    },
+  );
+  results[i] = result;
+  console.log(
+    `(${i + 1}):${data.multiplets[i].name}  ${
+      data.multiplets[i].category
+    } from ${fromPt} to ${toPt} , ${data.multiplets[i].rangeMin} - ${
+      data.multiplets[i].rangeMax
+    } phase : ${result.phaseCorrectionOnMultipletInDeg} `,
+  );
+}
+
+writeFileSync(
+  join(__dirname, '../web', 'results_mn.json'),
+  JSON.stringify(results, null, 2),
+  'utf8',
+);
+console.log(result);
