d1e006e93a47ba0fb64a63135d4ea8fc1dc24bb4
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-03-22 18:19:40 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import { analyseMultiplet } from '/Volumes/san256/users_for_mac_system_macPro/jeannerat/mygit/multiplet-analysis/src/index';
+
 import { writeFileSync } from 'fs';
 import { join } from 'path';
 
 export function myModule(data = {}) {
   let result = analyseMultiplet(data, {
-  frequency: 600,
-  debug: true,
-  takeBestPartMultiplet: true,
-  appliedPhaseCorrectionType: 1,
-});
+    frequency: 600,
+    debug: true,
+    takeBestPartMultiplet: true,
+    appliedPhaseCorrectionType: 1,
+  });
 
-writeFileSync(
-  join(__dirname, '../web', 'result.json'),
-  JSON.stringify(result, null, 2),
-  'utf8',
-);
+  writeFileSync(
+    join(__dirname, '../web', 'result.json'),
+    JSON.stringify(result, null, 2),
+    'utf8',
+  );
 
-console.log(result);
-return 42
+  console.log(result);
+  return 42;
 }
